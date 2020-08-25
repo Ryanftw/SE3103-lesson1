3e434554afbe8ead68a97f3f83099d7fc27400f0
Ryan <rlorenz@uco.edu>
2020-08-25 13:37:51 -0500

    Calculator class complete

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -3,12 +3,27 @@ package test;
 import model.Calculator;
 
 public class CalculatorTest {
+
 	public static void main(String[] args) {
 		Calculator c1 = new Calculator();
 		c1.enter(1);
 		c1.enter(2);
 		c1.add();
 
-		assert c1.getResult() == 2;
+		assert c1.getResult() == 3;
+
+		c1.multiply();
+		assert c1.getResult() == 1 * 2;
+		c1.enter(2);
+		c1.enter(3);
+		c1.multiply();
+		assert c1.getResult() == 2 * 3;
+
+		c1.subtract();
+		assert c1.getResult() == 3 - 2;
+
+		c1.divide();
+		assert c1.getResult() == 3.0 / 2;
+
 	}
 }
